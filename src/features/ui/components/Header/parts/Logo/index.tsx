02e10a4cae6fa727506d3f5627d8178715528932
Odusseus55 <odusseus.development@gmail.com>
2022-05-28 22:33:29 +0200

    chore: add types

--- src/features/ui/components/Header/parts/Logo/index.tsx
+++ src/features/ui/components/Header/parts/Logo/index.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import type { FC } from 'react'
 
 import { Routes } from '~/features/core/constants/routes'
 
-export const Logo = () => (
+export const Logo: FC = () => (
   <Link href={Routes.DASHBOARD}>
     {/*  eslint-disable-next-line jsx-a11y/anchor-is-valid */}
     <a>E.</a>
--- src/features/ui/components/Layout/index.tsx
+++ src/features/ui/components/Layout/index.tsx
@@ -1,8 +1,14 @@
+import type { ReactNode, FC } from 'react'
+
 import { Header } from '~/features/ui/components/Header'
 // FC - function components
 
+type Props = {
+  children: NonNullable<ReactNode>
+}
+
 // @ts-ignore // TODO: Add types in TS lesson
-export const Layout = ({ children }) => (
+export const Layout: FC<Props> = ({ children }) => (
   <>
     <Header />
     <main>{children}</main>
