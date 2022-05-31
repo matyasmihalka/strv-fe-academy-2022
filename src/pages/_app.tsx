585f9cbca47a3b106d72dd252b4b6366fece84a3
Odusseus55 <odusseus.development@gmail.com>
2022-05-31 15:10:49 +0200

    feat: add HeadDefault with meta and assets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '~/features/ui/theme/globals.css'
 import type { AppProps } from 'next/app'
 
+import { HeadDefault } from '~/features/core/components/HeadDefault'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeadDefault />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
