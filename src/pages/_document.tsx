131aa9fe4c2c9389af27e89c15156ac483efe9e4
Odusseus55 <odusseus.development@gmail.com>
2022-06-12 11:11:56 +0200

    feat: adding styles config on SSR to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,35 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import type { DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
+import { ServerStyleSheet } from 'styled-components'
 
-type Props = {}
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-class Document extends NextDocument<Props> {
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
   render() {
     return (
       <Html>
@@ -34,4 +60,4 @@ class Document extends NextDocument<Props> {
   }
 }
 
-export default Document
+export default MyDocument
