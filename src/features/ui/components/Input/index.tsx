86249a0b00df878985c34c6949143cfdefa75d48
Odusseus55 <odusseus.development@gmail.com>
2022-06-22 18:54:16 +0200

    feat: login page form switched to react-hook-forms

--- src/features/ui/components/Input/index.tsx
+++ src/features/ui/components/Input/index.tsx
@@ -1,4 +1,5 @@
 import type { FC, InputHTMLAttributes } from 'react'
+import { forwardRef } from 'react'
 import { useState } from 'react'
 
 import { TogglePasswordIcon } from './parts/TogglePasswordIcon'
@@ -16,32 +17,35 @@ type Props = InputHTMLAttributes<HTMLInputElement> & {
   error?: string | null
 }
 
-export const Input: FC<Props> = ({ label, name, type, error, ...rest }) => {
-  const [isPasswordShown, togglePassword] = useState(false)
-  const inputType = isPasswordShown ? 'text' : type
+export const Input: FC<Props> = forwardRef<HTMLInputElement, Props>(
+  ({ label, type, error, ...rest }, ref) => {
+    const [isPasswordShown, togglePassword] = useState(false)
+    const inputType = isPasswordShown ? 'text' : type
 
-  return (
-    <InputWrapper>
-      <StyledLabel hasError={Boolean(error)}>
-        <StyledInput
-          placeholder={label}
-          name={name}
-          type={inputType}
-          {...rest}
-        />
-        <LabelText>{label}</LabelText>
-        {!!error && <ErrorMessage>{error}</ErrorMessage>}
-        {type === 'password' && (
-          <ToggleButton
-            type="button"
-            aria-label="Display password text"
-            onClick={() => togglePassword((prevState) => !prevState)}
-          >
-            <TogglePasswordIcon />
-          </ToggleButton>
-        )}
-      </StyledLabel>
-      {/* eslint-disable-next-line react/forbid-dom-props */}
-    </InputWrapper>
-  )
-}
+    return (
+      <InputWrapper>
+        <StyledLabel hasError={Boolean(error)}>
+          <StyledInput
+            placeholder={label}
+            type={inputType}
+            ref={ref}
+            {...rest}
+          />
+          <LabelText>{label}</LabelText>
+          {!!error && <ErrorMessage>{error}</ErrorMessage>}
+          {type === 'password' && (
+            <ToggleButton
+              type="button"
+              aria-label="Display password text"
+              onClick={() => togglePassword((prevState) => !prevState)}
+            >
+              <TogglePasswordIcon />
+            </ToggleButton>
+          )}
+        </StyledLabel>
+      </InputWrapper>
+    )
+  }
+)
+
+Input.displayName = 'Input'
