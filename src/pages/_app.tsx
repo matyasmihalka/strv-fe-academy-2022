beaecef33e93fa3d65152cd2894f95b3b59b85bf
Odusseus55 <odusseus.development@gmail.com>
2022-07-08 22:57:45 +0200

    feat: error  boundary added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { UserContextProvider } from '~/features/auth/contexts/userContext'
 import { HeadDefault } from '~/features/core/components/HeadDefault'
+import { ErrorBoundary } from '~/features/core/components/HeadDefault/ErrorBoundary'
 import { EventFilterContextProvider } from '~/features/events/contexts/event-filter'
 import { EventViewContextProvider } from '~/features/events/contexts/event-view'
 import { GlobalStyle } from '~/features/ui/theme/global'
@@ -13,20 +14,22 @@ const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ErrorBoundary type="next_root">
       <GlobalStyle />
       <HeadDefault />
       <QueryClientProvider client={queryClient}>
-        <UserContextProvider>
-          <EventViewContextProvider>
-            <EventFilterContextProvider>
-              <Component {...pageProps} />
-            </EventFilterContextProvider>
-          </EventViewContextProvider>
-        </UserContextProvider>
+        <ErrorBoundary type="app_root">
+          <UserContextProvider>
+            <EventViewContextProvider>
+              <EventFilterContextProvider>
+                <Component {...pageProps} />
+              </EventFilterContextProvider>
+            </EventViewContextProvider>
+          </UserContextProvider>
+        </ErrorBoundary>
         <ReactQueryDevtools />
       </QueryClientProvider>
-    </>
+    </ErrorBoundary>
   )
 }
 
